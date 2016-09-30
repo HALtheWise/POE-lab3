5b2e384fc4af143710bf68201eb9ba1f6f252449
Eric Miller <eric@legoaces.org>
2016-09-30 13:14:15 -0400

    Added motor control capability to v1 code

---
+++ version-1/libraries.md
@@ -0,0 +1,5 @@
+# Libraries needed to build
+
+In order to successfully build and install this program, you need to copy the following libraries into your sketchbook libraries directory (`~/Arduino/libraries` on Linux)
+
+- [Adafruit v2 motor shield library](https://learn.adafruit.com/adafruit-motor-shield-v2-for-arduino/install-software)
--- version-1/version-1.ino
+++ version-1/version-1.ino
@@ -1,19 +1,30 @@
 /* 
-*	Sensor calibration code for Lab 3 of POE Fall 2016 as taught at Olin College
-*	This code averages sensor readings over a period of 200ms, then communicates that
-*		data out on the serial port with format "leftSensorAvg,rightSensorAvg\n"
+*	Version 1 Arduino code for Lab 3 of POE Fall 2016 as taught at Olin College
+*	This code demonstrates reading the sensor values and controlling the motors
+*	using a trivial bang-bang control scheme. 
+*	This is not intended to be final code, but merely a proof of concept.
 *
 *	Authors: Eric Miller (eric@legoaces.org) and Jamie Cho (yoonyoung.cho@students.olin.edu)
 */
 
 #include <stdlib.h>
 
-const int averagingDuration = 200;
+// Imports for Motor Shield, taken from https://learn.adafruit.com/adafruit-motor-shield-v2-for-arduino/using-dc-motors
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+#include "utility/Adafruit_MS_PWMServoDriver.h"
 
-const byte leftSensorPin = A0;
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+
+
+const int averagingDuration = 10; //This is the inverse of the main loop time
+
+const byte leftSensorPin  = A0;
 const byte rightSensorPin = A1;
-// const byte leftMotorPin = 9;
-// const byte rightMotorPin = 10;
+
+Adafruit_DCMotor *leftMotor  = AFMS.getMotor(1);
+Adafruit_DCMotor *rightMotor = AFMS.getMotor(2);
+
 
 long lastActionTime;
 
@@ -25,6 +36,8 @@ void setup()
 
 	// Initialize pins
 	// Note that the analog sensors don't need initialization
+	AFMS.begin();
+
 }
 
 long totalLeft = 0;
@@ -62,6 +75,24 @@ void writeSerial(float leftAvg, float rightAvg)
 	Serial.print(",");
 	Serial.print(rightAvg);
 	Serial.print("\n");
+}
+
+void driveMotors(int leftPower, int rightPower){
+	// Inputs leftPower and rightPower vary from -255...255
+	// Code in this function is based on https://learn.adafruit.com/adafruit-motor-shield-v2-for-arduino/using-dc-motors
+
+	// For each motor, decide whether to run it FORWARD, BACKWARD, (or RELEASE)
+	byte leftDirection	= (leftPower  > 0) ? FORWARD : BACKWARD;
+	byte rightDirection	= (rightPower > 0) ? FORWARD : BACKWARD;
+	
+	// Set motor speeds
+	leftMotor	->	setSpeed(abs(leftPower));
+	rightMotor	->	setSpeed(abs(rightPower));
+
+	// Set motor directions
+	leftMotor	->	run(leftDirection);
+	rightMotor	->	run(rightDirection);
+
 }
 
 void getMeasurements(int *leftRead, int *rightRead)
