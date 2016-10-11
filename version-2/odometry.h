8d23c695633d0bd92e9ad75dc0c510d2263122e9
Eric Miller <eric@legoaces.org>
2016-10-11 09:31:57 -0400

    Improved motor calibration to be slightly sublinear

--- version-2/odometry.h
+++ version-2/odometry.h
@@ -31,16 +31,17 @@ void Pose::reset(){
 const double MAX_FORWARD_SPEED = 100;
 const double MAX_TURN_SPEED = 500;
 
-int adjustPower( int power ){
-	return power-3;
+double adjustPower( int power ){
+	double powerfrac = power / 255.0
+	return powerfrac - .05 * powerfrac * powerfrac;
 }
 
 // Update the estimated pose of the robot based on wheel (estimated) odometry.
 void Pose::odometryUpdate( int leftPower, int rightPower, int timestep) {
-	leftPower  = adjustPower(leftPower);
-	rightPower = adjustPower(rightPower);
-	double forwardSpeed = (leftPower + rightPower) / 2 * MAX_FORWARD_SPEED / 255.0;
-	double turnSpeed = (rightPower - leftPower) / 2 * MAX_TURN_SPEED / 255.0;
+	double leftPowerFrac  = adjustPower(leftPower);
+	double rightPowerFrac = adjustPower(rightPower);
+	double forwardSpeed = (leftPower + rightPower) / 2 * MAX_FORWARD_SPEED;
+	double turnSpeed = (rightPower - leftPower) / 2 * MAX_TURN_SPEED;
 
 	distAlong += forwardSpeed * timestep / 1000.0;
 	angleFrom += turnSpeed * timestep / 1000.0;
